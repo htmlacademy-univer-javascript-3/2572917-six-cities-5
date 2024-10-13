e90e57372fb481f0155ab9f003b7dc0c0b1dd637
VladimirSharp <zhiltsov.v.a@yandex.ru>
2024-10-13 12:11:58 +0300

    Закончено второе задание

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import { Main } from './pages/Main';
+import {IPlaceCard} from './types';
+
+type AppScreenProps = {
+  places: IPlaceCard[];
+}
+
+export const App: React.FC<AppScreenProps> = ({places}) => (
+  <Main places={places}/>
+);
