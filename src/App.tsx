361563bed58af332fcf503a6f804e3b56efd637f
VladimirSharp <zhiltsov.v.a@yandex.ru>
2024-10-20 13:36:16 +0300

    Настроены маршруты

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 ﻿import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Main } from './pages/Main/Main';
-import {IPlaceCard} from './types';
+import { IPlaceCard } from './types';
+import CheckAuth from './pages/CheckAuth/CheckAuth';
+import { Favorites } from './pages/Favorites/Favorites';
+import { Offer } from './pages/Offer/Offer';
+import { NotFound } from './pages/Errors/NotFound';
+import { Login } from './pages/Login';
 
 type AppProps = {
   places: IPlaceCard[];
 }
 
-export const App: React.FC<AppProps> = ({places}) => (
-  <Main places={places}/>
+export const App: React.FC<AppProps> = ({ places }) => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main places={places} />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/favorites" element={<CheckAuth element={<Favorites />} isAuthorized={false}></CheckAuth>} />
+      <Route path="/offer/:id" element={<Offer />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
